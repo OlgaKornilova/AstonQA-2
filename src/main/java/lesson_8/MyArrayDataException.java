cbf7413ed30e2846598295ea97434c8f2264f316
o-kornilova <kornilova.s.olga@gmail.com>
2024-05-14 13:11:24 +0200

    lesson 8. Separate classes to files

---
+++ src/main/java/lesson_8/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package lesson_8;
+
+public class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/lesson_8/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package lesson_8;
+
+public class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
